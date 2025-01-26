0286428924e3d9dfda3ff42098e6fd56a96997aa
v_almeida <119812308+Sheridanuwu@users.noreply.github.com>
2025-01-26 17:54:10 -0400

    Tela de Cadastro finalizada

--- app/src/main/java/com/medsync/medsync/TelaMenu.kt
+++ app/src/main/java/com/medsync/medsync/TelaMenu.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -22,11 +23,11 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.layout.wrapContentWidth
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.rounded.Face
 import androidx.compose.material.icons.rounded.Menu
@@ -44,10 +45,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -57,9 +57,14 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navOptions
 import com.example.medsync.R
-import com.medsync.medsync.ui.theme.DarkWhite
 import com.medsync.medsync.ui.theme.MedSyncTheme
+
 
 data class BottomNavigationItem(
     val title: String,
@@ -76,76 +81,67 @@ class TelaMenu : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MedSyncTheme {
-                val items = listOf(
-                    BottomNavigationItem(
-                        title = "Menu",
-                        selectedIcon = Icons.Filled.Home,
-                        unselectedIcon = Icons.Filled.Home,
-                        hasNews = false
-                    ),
-                    BottomNavigationItem(
-                        title = "Perfil",
-                        selectedIcon = Icons.Filled.Home,
-                        unselectedIcon = Icons.Filled.Home,
-                        hasNews = false
-                    ),
-                )
+                val navController = rememberNavController()
+                Surface(color = MaterialTheme.colorScheme.background) {
 
-                var selectedItemIndex by rememberSaveable {
-                    mutableStateOf(0)
-                }
-
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Scaffold(
-                        bottomBar = {
-                            NavigationBar {
-                                items.forEachIndexed { index, item ->
-                                    NavigationBarItem(
-                                        selected = selectedItemIndex == index,
-                                        onClick = {
-                                            selectedItemIndex = index
-                                            //  navController.navigate(item.title))
-                                        },
-                                        label = {
-                                            Text(text = item.title)
-                                        },
-                                        icon = {
-                                            BadgedBox(badge = {
-                                                if(item.badgeCount != null) {
-                                                    Badge{
-                                                        Text(text = item.badgeCount.toString())
-                                                    }
-                                                } else if(item.hasNews){
-                                                    Badge()
-                                                }
-                                            }
-                                            ) {
-                                                Icon(
-                                                    imageVector = if(index == selectedItemIndex) {
-                                                        item.selectedIcon
-                                                    } else item.unselectedIcon,
-                                                    contentDescription = item.title
-                                                )
-                                            }
-                                        }
-                                    )
-                                }
-                            }
-                        }
-                    ) {
-
+                    val itens = remember {
+                        listOf(
+                            Pair("Menu", Icons.Filled.Home),
+                            Pair("Perfil", Icons.Filled.Person)
+                        )
                     }
-                }
+                    var selectedItem by remember {
+                        mutableStateOf(itens.first())
+                    }
 
-            }
+                    Column {
 
+                        NavHost(
+                            navController = navController,
+                            startDestination = "telaA",
+                            Modifier.weight(1f)
+                        ) {
+                            composable("telaA") {
+                                Menu()
+                            }// fim do composable
 
+                            composable("telaB") {
+                                Perfil()
+                            } // fim do composable
 
+                        }// fim do navhost
+                        BottomAppBar(actions = {
+                            itens.forEach { item ->
+                                val text = item.first
+                                val icon = item.second
+                                NavigationBarItem(
+                                    selected = selectedItem == item,
+                                    onClick = {
+                                        selectedItem = item
+                                        //nao volta pra tela A, só vai pra B
+                                        val route = when(text){
+                                            "tela A" -> "telaA"
+                                            else -> "telaB"
+                                        }
+                                        navController.navigate(route, navOptions = navOptions {
+                                            launchSingleTop = true
+                                            popUpTo(navController.graph.startDestinationId)
+                                        })
+                                    },
+                                    icon = {
+                                        Icon(icon, contentDescription = null)
+                                    }, // fim icons
+                                    label = {
+                                        Text(text = text)
+                                    })// fim navigationbarritem
+                            }//Fim do forEach
+                        })// fim bottomAppBar
+                    }// fim do column
 
-            Menu()
+                }//fim do surface
+
+            }// fim do theme
+
         }
     }
 }
@@ -159,76 +155,150 @@ fun Menu() {
     var intentMenu = Intent(context, TelaMenu::class.java)
     var intentPerfil = Intent(context, TelaPerfil::class.java)
 
-    Column(modifier = Modifier
-        .fillMaxSize()
-        .verticalScroll(ScrollState(0))
-        .background(Color(0xFFD7D6D6))
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .verticalScroll(ScrollState(0))
+            .background(Color(0xFFD7D6D6))
     ) {
 
 //        Cabeçalho
-    Surface(
-        Modifier
-            .fillMaxWidth()
-            .height(200.dp), // TO-DO tem que ajustar
-        color = Color.Cyan
-    ) {  }
+        Surface(
+            Modifier
+                .fillMaxWidth()
+                .height(200.dp), // TO-DO tem que ajustar
+            color = Color.Cyan
+        ) { }
 
-    // Menu de fato
+        // Menu de fato
 //      Coluna amarela -> Creio eu que so as Box sejam de cor diferentes, já que isso pensei pra ser o fundo sombreado.
 //      Pra dar aquela sensação de icone flutuante
-        Column(Modifier.fillMaxSize().weight(1f).background(color = Color.Yellow).padding(10.dp), horizontalAlignment = Alignment.CenterHorizontally)
+        Column(
+            Modifier
+                .fillMaxSize()
+                .weight(1f)
+                .background(color = Color.Yellow)
+                .padding(10.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        )
         {
 //            Fila verde, 2 comp.
-            Row(Modifier.background(color = Color.Green).fillMaxWidth().padding(10.dp), horizontalArrangement = Arrangement.SpaceBetween) {
-                Box(Modifier.background(Color.Red).weight(1f).height(100.dp).clickable {  }.padding(10.dp),){
+            Row(
+                Modifier
+                    .background(color = Color.Green)
+                    .fillMaxWidth()
+                    .padding(10.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Box(
+                    Modifier
+                        .background(Color.Red)
+                        .weight(1f)
+                        .height(100.dp)
+                        .clickable { }
+                        .padding(10.dp),
+                ) {
 //                    Creio ser necessário ajustar isso quando colocar o texto definiito e a imagem
-                    Row(){
-                        Icon(imageVector = Icons.Rounded.Menu, contentDescription = "MedSync") // TO-DO tem que colocar uma imagem aqui
-                        Text(text = "Olá MedSync!",)
+                    Row() {
+                        Icon(
+                            imageVector = Icons.Rounded.Menu,
+                            contentDescription = "MedSync"
+                        ) // TO-DO tem que colocar uma imagem aqui
+                        Text(text = "Olá MedSync!")
                     }
                 }
                 Spacer(Modifier.width(10.dp))
-                Box(Modifier.background(Color.Red).weight(1f).height(100.dp).clickable {  }.padding(10.dp),){
+                Box(
+                    Modifier
+                        .background(Color.Red)
+                        .weight(1f)
+                        .height(100.dp)
+                        .clickable { }
+                        .padding(10.dp),
+                ) {
 //                    Creio ser necessário ajustar isso quando colocar o texto definiito e a imagem
-                    Row(){
-                        Icon(imageVector = Icons.Rounded.Menu, contentDescription = "MedSync") // TO-DO tem que colocar uma imagem aqui
-                        Text(text = "Olá MedSync!",)
+                    Row() {
+                        Icon(
+                            imageVector = Icons.Rounded.Menu,
+                            contentDescription = "MedSync"
+                        ) // TO-DO tem que colocar uma imagem aqui
+                        Text(text = "Olá MedSync!")
                     }
                 }
             }
-            Row(Modifier.background(color = Color.Green).fillMaxWidth().padding(10.dp), horizontalArrangement = Arrangement.SpaceBetween) {
-                Box(Modifier.background(Color.Red).weight(1f).height(100.dp).clickable {  }.padding(10.dp),){
+            Row(
+                Modifier
+                    .background(color = Color.Green)
+                    .fillMaxWidth()
+                    .padding(10.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Box(
+                    Modifier
+                        .background(Color.Red)
+                        .weight(1f)
+                        .height(100.dp)
+                        .clickable { }
+                        .padding(10.dp),
+                ) {
 //                    Creio ser necessário ajustar isso quando colocar o texto definiito e a imagem
-                    Row(){
-                        Icon(imageVector = Icons.Rounded.Menu, contentDescription = "MedSync") // TO-DO tem que colocar uma imagem aqui
-                        Text(text = "Olá MedSync!",)
+                    Row() {
+                        Icon(
+                            imageVector = Icons.Rounded.Menu,
+                            contentDescription = "MedSync"
+                        ) // TO-DO tem que colocar uma imagem aqui
+                        Text(text = "Olá MedSync!")
                     }
                 }
                 Spacer(Modifier.width(10.dp))
-                Box(Modifier.background(Color.Red).weight(1f).height(100.dp).clickable {  }.padding(10.dp),){
+                Box(
+                    Modifier
+                        .background(Color.Red)
+                        .weight(1f)
+                        .height(100.dp)
+                        .clickable { }
+                        .padding(10.dp),
+                ) {
 //                    Creio ser necessário ajustar isso quando colocar o texto definiito e a imagem
-                    Row(){
-                        Icon(imageVector = Icons.Rounded.Menu, contentDescription = "MedSync") // TO-DO tem que colocar uma imagem aqui
-                        Text(text = "Olá MedSync!",)
+                    Row() {
+                        Icon(
+                            imageVector = Icons.Rounded.Menu,
+                            contentDescription = "MedSync"
+                        ) // TO-DO tem que colocar uma imagem aqui
+                        Text(text = "Olá MedSync!")
                     }
                 }
             }
 
-            Row(Modifier.background(color = Color.Green).fillMaxWidth().padding(10.dp), horizontalArrangement = Arrangement.SpaceBetween) {
-                Box(Modifier.background(Color.Red).fillMaxWidth().height(100.dp).clickable {  }.padding(10.dp),) {
-                    Row(){
-                        Icon(imageVector = Icons.Rounded.Menu, contentDescription = "MedSync") // TO-DO tem que colocar uma imagem aqui
-                        Text(text = "Olá MedSync!",)
+            Row(
+                Modifier
+                    .background(color = Color.Green)
+                    .fillMaxWidth()
+                    .padding(10.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Box(
+                    Modifier
+                        .background(Color.Red)
+                        .fillMaxWidth()
+                        .height(100.dp)
+                        .clickable { }
+                        .padding(10.dp),
+                ) {
+                    Row() {
+                        Icon(
+                            imageVector = Icons.Rounded.Menu,
+                            contentDescription = "MedSync"
+                        ) // TO-DO tem que colocar uma imagem aqui
+                        Text(text = "Olá MedSync!")
                     }
                 }
             }
-
 
 
         }   // FIM DO COLUMN DE OPÇÕES
-            // SE FOR NECESSÁRIO MAIS CAIXAS, É SÓ COPIAR OS DE CIMA =)
-
-
+        // SE FOR NECESSÁRIO MAIS CAIXAS, É SÓ COPIAR OS DE CIMA =)
 
 
 //        barra de navegação bottom
--- app/src/main/java/com/medsync/medsync/TelaPerfil.kt
+++ app/src/main/java/com/medsync/medsync/TelaPerfil.kt
@@ -18,30 +18,11 @@ class TelaPerfil : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MedSyncTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MedSyncTheme {
-        Greeting("Android")
-    }
+fun Perfil(){
+    Text(text = "Oláa")
 }
